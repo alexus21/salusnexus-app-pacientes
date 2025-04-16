d72839478d8722d3178f7ab2c5687bcdd5bf7c70
Alex Ulloa <anders_nielsen_0727@proton.me>
2025-04-15 21:32:02 -0600

    Sidebar

--- src/components/userprofile/UserProfileComponent.vue
+++ src/components/userprofile/UserProfileComponent.vue
@@ -1,99 +1,138 @@
 <template>
-    <div class="row m-5">
-        <div class="col-md-4">
-            <div class="row d-flex justify-content-center">
+    <div class="row">
+        <div class="col-md-2">
+            <div class="row">
                 <div class="row d-flex justify-content-center">
-                    <img :src="profile_photo" alt="profile picture" class="img-fluid">
-                </div>
-                <div class="row d-flex justify-content-center mt-3">
-                    <p class="p-name text-primary">{{
-                            user ? user.first_name.split(' ')[0] + ' ' + user.last_name.split(' ')[0] : 'Cargando...'
-                        }}</p>
-                </div>
-                <div class="col d-flex justify-content-center mt-3">
-                    <button class="btn btn-outline-light border border-black border-1 text-black" id="btnCancelar"
-                            @click="handleCancelEditProfile">
-                        Cancelar
-                    </button>
-                    <button class="btn btn-primary" id="btnEditar" @click="handleEditProfile">Editar</button>
+                    <div class="row d-flex justify-content-center">
+                        <img :src="profile_photo" alt="profile picture" class="img-fluid">
+                    </div>
+                    <div class="row d-flex justify-content-center mt-3">
+                        <p class="fw-bold">{{
+                                user ? user.first_name.split(' ')[0] + ' ' + user.last_name.split(' ')[0] : 'Cargando...'
+                            }}</p>
+                    </div>
+                    <div class="row d-flex justify-content-center">
+                        <p class="text-secondary">{{
+                                user ? user.email : 'Cargando...'
+                            }}</p>
+                    </div>
+                    <div class="row d-flex justify-content-center mt-3">
+                        <button class="edit-profile-button"
+                                id="btnEditar"
+                                @click="handleEditProfile">
+                            Editar
+                        </button>
+                    </div>
                 </div>
             </div>
-            <div class="row d-flex justify-content-center mt-5">
-                <div class="col d-flex justify-content-center mt-3">
-                    <p>
-                        Plan actual: {{ user ? subscription_type : 'Cargando...' }}
-                    </p>
-                </div>
-
-                <div class="col d-flex justify-content-center mt-3">
-                    <button class="btn btn-primary"
-                            id="btnActualizarPlan"
-                            v-if="subscription_type"
-                            @click="handleSwitchSubscription">
-                        {{ SubscriptionButtonText }}
-                    </button>
-                </div>
+            <hr/>
+            <div class="row">
+                <button class="sidebar-buttons">
+                    <i class="fa-solid fa-user me-2"></i>
+                    Mi Perfil
+                </button>
+                <button class="sidebar-buttons">
+                    <i class="fa-solid fa-credit-card me-2"></i>
+                    Mi Suscripción</button>
+                <button class="sidebar-buttons">
+                    <i class="fa-solid fa-shield me-2"></i>
+                    Seguridad
+                </button>
+                <button class="sidebar-buttons">
+                    <i class="fa-solid fa-bell me-2"></i>
+                    Notificaciones
+                </button>
+                <button class="sidebar-buttons">
+                    <i class="fa-solid fa-gear me-2"></i>
+                    Configuración
+                </button>
             </div>
         </div>
+        <div class="col-md-auto">
+            <div class="row m-5">
+                <!--            <div class="col-md-4">
+																<div class="row d-flex justify-content-center mt-5">
+																		<div class="col d-flex justify-content-center mt-3">
+																				<p>
+																						Plan actual: {{ user ? subscription_type : 'Cargando...' }}
+																				</p>
+																		</div>
 
-        <div class="col-md-8">
-            <div class="row d-flex justify-content-center mt-5 text-start">
-                <h3>Información personal</h3>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item d-flex align-items-center">
-                        <span class="material-icons text-primary me-3">person </span>
-                        <input type="text"
-                               class="form-control w-50"
-                               :value="user ? user.first_name.split(' ')[0] + ' ' + user.last_name.split(' ')[0] : 'Cargando...'"
-                               :readonly="readonly">
-                    </li>
-                    <li class="list-group-item d-flex align-items-center">
-                        <span class="material-icons text-primary me-3">email </span>
-                        <input type="text"
-                               class="form-control w-50"
-                               :value="user ? user.email : 'Cargando...'"
-                               readonly>
-                    </li>
-                    <li class="list-group-item d-flex align-items-center">
-                        <span class="material-icons text-primary me-3">phone </span>
-                        <input type="text"
-                               class="form-control w-50"
-                               :value="user ? user.phone : 'Cargando...'"
-                               :readonly="readonly">
-                    </li>
-                    <li class="list-group-item d-flex align-items-center">
-                        <span class="material-icons text-primary me-3">home </span>
-                        <input type="text"
-                               class="form-control w-50"
-                               :value="user ? user.home_address : 'Cargando...'"
-                               :readonly="readonly">
-                    </li>
-                    <li class="list-group-item d-flex align-items-center">
-                        <span class="material-icons text-primary me-3">emergency </span>
-                        <input type="text"
-                               class="form-control w-50"
-                               :value="user ? user.emergency_contact_name : 'Cargando...'"
-                               :readonly="readonly">
-                    </li>
-                    <li class="list-group-item d-flex align-items-center">
-                        <span class="material-icons text-primary me-3">contact_emergency </span>
-                        <input type="text"
-                               class="form-control w-50"
-                               :value="user ? user.emergency_contact_phone : 'Cargando...'"
-                               :readonly="readonly">
-                    </li>
-                    <li class="list-group-item d-flex align-items-center">
-                        <span class="material-icons text-primary me-3">calendar_month</span>
-                        <input type="date"
-                               class="form-control w-50"
-                               :value="user ? user.date_of_birth : 'Cargando...'"
-                               :readonly="readonly">
-                    </li>
-                </ul>
-            </div>
-            <div class="row d-flex justify-content-center mt-5 text-start">
-                <h3>Información de ubicación</h3>
+																		<div class="col d-flex justify-content-center mt-3">
+																				<button class="btn btn-primary"
+																								id="btnActualizarPlan"
+																								v-if="subscription_type"
+																								@click="handleSwitchSubscription">
+																						{{ SubscriptionButtonText }}
+																				</button>
+																		</div>
+																</div>
+														</div>-->
 
+                <div class="col-md-8">
+                    <div class="row">
+                        <h1>Mi perfil</h1>
+                        <p>Gestionar tu información personal</p>
+                    </div>
+                    <div class="row d-flex justify-content-center mt-5 text-start">
+                        <h3>Información personal</h3>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item d-flex align-items-center">
+                                <span class="material-icons text-primary me-3">person </span>
+                                <input type="text"
+                                       class="form-control w-50"
+                                       :value="user ? user.first_name.split(' ')[0] + ' ' + user.last_name.split(' ')[0] : 'Cargando...'"
+                                       :readonly="readonly">
+                            </li>
+                            <li class="list-group-item d-flex align-items-center">
+                                <span class="material-icons text-primary me-3">email </span>
+                                <input type="text"
+                                       class="form-control w-50"
+                                       :value="user ? user.email : 'Cargando...'"
+                                       readonly>
+                            </li>
+                            <li class="list-group-item d-flex align-items-center">
+                                <span class="material-icons text-primary me-3">phone </span>
+                                <input type="text"
+                                       class="form-control w-50"
+                                       :value="user ? user.phone : 'Cargando...'"
+                                       :readonly="readonly">
+                            </li>
+                            <li class="list-group-item d-flex align-items-center">
+                                <span class="material-icons text-primary me-3">home </span>
+                                <input type="text"
+                                       class="form-control w-50"
+                                       :value="user ? user.home_address : 'Cargando...'"
+                                       :readonly="readonly">
+                            </li>
+                            <li class="list-group-item d-flex align-items-center">
+                                <span class="material-icons text-primary me-3">emergency </span>
+                                <input type="text"
+                                       class="form-control w-50"
+                                       :value="user ? user.emergency_contact_name : 'Cargando...'"
+                                       :readonly="readonly">
+                            </li>
+                            <li class="list-group-item d-flex align-items-center">
+                                <span class="material-icons text-primary me-3">contact_emergency </span>
+                                <input type="text"
+                                       class="form-control w-50"
+                                       :value="user ? user.emergency_contact_phone : 'Cargando...'"
+                                       :readonly="readonly">
+                            </li>
+                            <li class="list-group-item d-flex align-items-center">
+                                <span class="material-icons text-primary me-3">calendar_month</span>
+                                <input type="date"
+                                       class="form-control w-50"
+                                       :value="user ? user.date_of_birth : 'Cargando...'"
+                                       :readonly="readonly">
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="row d-flex justify-content-center mt-5 text-start">
+                        <h3>Información de ubicación</h3>
+
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -179,29 +218,18 @@ export default {
 <style scoped>
 img {
     border-radius: 50%;
-    width: 300px; /* Ajusta el tamaño según sea necesario */
-    height: 250px; /* Ajusta el tamaño según sea necesario */
-    object-fit: cover; /* Asegura que la imagen se recorte para llenar el contenedor */
+    width: 150px;
+    height: 125px;
+    object-fit: cover;
 }
 
 .p-name {
-    font-size: 35px;
+    font-size: 15px;
     font-weight: bold;
     font-family: 'Arial Black', sans-serif, 'Comic Sans MS', cursive;
 }
 
 #btnCancelar {
-    display: flex;
-    flex-wrap: nowrap;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    width: 200px;
-    height: 50px;
-}
-
-#btnEditar {
-    margin-left: 15px;
     display: flex;
     flex-wrap: nowrap;
     justify-content: center;
@@ -224,6 +252,36 @@ input:focus {
 
 .list-group-item {
     border-bottom: none !important;
+}
+
+.edit-profile-button{
+    background-color: #f0f7ff;
+    color: #4f5bf7;
+    border: #4f5bf7 solid 1px;
+    border-radius: 5px;
+    width: 200px;
+    height: 35px;
+}
+
+.edit-profile-button:hover {
+    background-color: #4f5bf7;
+    color: #ffffff;
+    font-weight: bold;
+}
+
+.sidebar-buttons {
+    background-color: #f0f7ff;
+    color: #6275f0;
+    border: 0;
+    height: 50px;
+    text-align: start;
+    padding-left: 10%;
+}
+
+.sidebar-buttons:hover {
+    background-color: #4f5bf7;
+    color: #ffffff;
+    font-weight: bold;
 }
 
 </style>
